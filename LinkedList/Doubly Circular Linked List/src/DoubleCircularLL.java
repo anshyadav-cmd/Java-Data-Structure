2d40326a7c97e2bb85cc28eddab28c17311e0a40
anshyadav <anshyadav4244@gmail.com>
2022-04-01 16:26:39 +0530

    doubly linked List added

--- LinkedList/Doubly Circular Linked List/src/DoubleCircularLL.java
+++ LinkedList/Doubly Circular Linked List/src/DoubleCircularLL.java
@@ -1,7 +1,7 @@
 public class DoubleCircularLL {
     private Node head;
     private Node tail;
-    private int size = 0;
+    public  int size = 0;
 
     public Node createDCLL (int value) {
         Node node = new Node();
@@ -13,5 +13,125 @@ public class DoubleCircularLL {
         size = 1;
         return head;
     }
+    public void insertion(int value, int position) {
+        Node node = new Node();
+        node.value = value;
+        if(head == null) {
+            createDCLL(value);
+            return;
+        }
+        if(position == 0) {
+            node.next = head;
+            node.prev = head.prev;
+            head.prev = node;
+            head = node;
+            tail.next = head;
+        }
+        else if(position >= size) {
+            node.next = tail.next;
+            node.prev = tail;
+            tail.next = node;
+            tail = node;
+            head.prev = tail;
+        }
+        else {
+            Node tempNode = head;
+            for(int i= 1 ; i < position; i++) {
+                tempNode = tempNode.next;
+            }
+            node.next = tempNode.next;
+            node.prev = tempNode;
+            tempNode.next.prev = node;
+            tempNode.next = node;
+        }
+        size++;
+    }
 
+    public void display() {
+        if(head == null) {
+            System.out.println("Linked list is empty or do not exists");
+            return;
+        }
+        Node tempNode = head;
+       while(tempNode.next != head) {
+           System.out.print(tempNode.value + " -> ");
+           tempNode = tempNode.next;
+       }
+        System.out.println(tempNode.value);
+    }
+
+    public void reverseDisplay() {
+        if(head == null) {
+            System.out.println("LInked list is empty or do not existis");
+            return;
+        }
+        Node tempNode = tail;
+        while(tempNode.prev != tail) {
+            System.out.print(tempNode.value + " -> ");
+            tempNode = tempNode.prev;
+        }
+        System.out.println(tempNode.value);
+    }
+
+    public boolean search(int value) {
+        if(head == null) {
+            System.out.println("Linked list is empty or do not exstis");
+            return false;
+        }
+        Node tempNode = head;
+        for(int i = 0; i < size ; i++) {
+            if(tempNode.value == value) {
+                return true;
+            }
+            tempNode = tempNode.next;
+        }
+        return false;
+    }
+
+    public void delete(int position) {
+        if(head == null) {
+            System.out.println("Linked list is empty or do not exists");
+            return;
+        }
+        if (head == tail) {
+            head.next =null;
+            head.prev = null;
+            head = null;
+            tail = null;
+        }else if(position == 0) {
+            head.prev = null;
+            head = head.next;
+            head.prev = tail;
+            tail.next = head;
+        }else if (position >= size) {
+            tail.next = null;
+            tail = tail.prev;
+            tail.next = head;
+            head.prev = tail;
+        }else {
+            Node tempNode = head;
+            for(int i =1 ; i < position; i++) {
+                tempNode = tempNode.next;
+            }
+            tempNode.next = tempNode.next.next;
+            tempNode.next.prev = tempNode;
+        }
+        size--;
+    }
+
+    public void deleteAll() {
+        if(head == null) {
+            System.out.println("Linked list is empty or do not exists");
+            return;
+        }
+        tail.next = null;
+        Node tempNode = head;
+        for(int i = 0; i < size; i++) {
+            tempNode.prev = null;
+            tempNode = tempNode.next;
+        }
+        head = null;
+        tail = null;
+        size = 0;
+    }
 }
--- LinkedList/Doubly Circular Linked List/src/Main.java
+++ LinkedList/Doubly Circular Linked List/src/Main.java
@@ -1,2 +1,40 @@
 public class Main {
+    public static void main(String[] args) {
+        DoubleCircularLL linkedList = new DoubleCircularLL();
+
+        linkedList.insertion(10, 2308);
+        linkedList.insertion(20, 10);
+        linkedList.insertion(50, 23423);
+        linkedList.insertion(30, 2);
+        linkedList.insertion(40, linkedList.size - 1);
+
+        linkedList.display();
+        linkedList.reverseDisplay();
+
+        linkedList.insertion(00 , 0);
+        linkedList.display();
+        linkedList.reverseDisplay();
+
+
+        System.out.println(linkedList.search(10));
+        System.out.println(linkedList.search(100));
+        System.out.println(linkedList.search(0));
+        System.out.println(linkedList.search(00));
+        System.out.println(linkedList.search(40));
+        System.out.println(linkedList.search(340));
+        System.out.println(linkedList.search(50));
+
+        linkedList.delete(0);
+        linkedList.display();
+
+        linkedList.delete(123);
+        linkedList.display();
+
+        linkedList.delete(3);
+        linkedList.display();
+
+        linkedList.deleteAll();
+        linkedList.display();
+
+    }
 }
