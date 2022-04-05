0e8aa704f7f21896abd7ce27edeaeb4f565bfa0a
anshyadav <anshyadav4244@gmail.com>
2022-04-05 12:26:22 +0530

    Queue

---
+++ Queue/Linear Queue Array/src/Queue.java
@@ -0,0 +1,13 @@
+public class Queue {
+    private int[] arr ;
+    private int beginningOfQueue;
+    private int topOfQueue;
+
+    public Queue (int size) {
+        arr = new int[size];
+        beginningOfQueue = -1;
+        topOfQueue = -1;
+    }
+
+    
+}
